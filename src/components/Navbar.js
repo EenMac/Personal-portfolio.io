5a0b9eebcc305a4663ac7748f40e1fde12b1cfc5
EenMacFullstack <Iain.mcdulling@gmail.com>
2022-06-03 13:21:58 +0100

    Navbar updated and various menu items have functionality and routes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,74 @@
-import React from 'react'
+import { React, useState} from 'react'
 import AppBar from '@mui/material/AppBar'
-import { Box, Icon, IconButton, MenuItem, Toolbar, Typography, Menu } from '@mui/material'
-
+import { Box, Icon, IconButton, MenuItem, Toolbar, Typography, Menu, Button} from '@mui/material'
+import { Container } from '@mui/system';
 
 function Navbar() {
+
+const [anchorEl, setAnchorEl] = useState(null);
+const open = Boolean(anchorEl);
+const handleMenu = event => {
+setAnchorEl(event.currentTarget);
+};
+const handleClose = () => {
+setAnchorEl(null);
+};
+
+
   return (
     <AppBar>
-        <Toolbar>
-            <IconButton>
-                Big I Dawg hamburger Icon
-            </IconButton>
-            <Menu>
-                <MenuItem>haazit</MenuItem>
-            </Menu>
-            <Typography>
-                Haazit
-            </Typography>
-        </Toolbar>
+        <Container>
+            <Toolbar>
+                <Box 
+                // sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
+                >
+                    <IconButton 
+                    onClick={handleMenu}
+                    >
+                        haazit
+                    </IconButton>
+                    <Menu
+                        anchorEl={anchorEl}
+                        // anchorOrigin={{
+                        // vertical: "bottom",
+                        // horizontal: "left"
+                        // }}
+                        open={Boolean(anchorEl)}
+                        onClose={handleClose}
+                        sx={{
+                        // display: { xs: "block", md: "none" }
+                        }}>
+                            <Button href="/">
+                                <MenuItem>Home</MenuItem>
+                            </Button>
+                            <Button href="/about">
+                                <MenuItem >About</MenuItem>
+                            </Button>
+                            <Button>
+                                <MenuItem href="/competencies">Competencies</MenuItem>
+                            </Button>
+                    </Menu>
+                </Box>
+                {/* <Typography
+                variant="h5"
+                noWrap
+                component="a"
+                href=""
+                // sx={{
+                //     mr: 2,
+                //     display: { xs: "flex", md: "none" },
+                //     flexGrow: 1,
+                //     fontFamily: "monospace",
+                //     fontWeight: 700,
+                //     letterSpacing: ".3rem",
+                //     color: "inherit",
+                //     textDecoration: "none"
+                // }}
+                >
+                LOGO
+                </Typography> */}
+            </Toolbar>
+        </Container>
     </AppBar>
   );
 };
