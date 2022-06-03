33499f73eeb7bcc09cdb1fbc205f9c88846128ca
EenMacFullstack <Iain.mcdulling@gmail.com>
2022-06-03 13:20:23 +0100

    App Updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from "./components/Home"
+import About from "./components/About"
+import Competencies from "./components/Competencies"
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Router>
+        <Navbar/>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/about" element={<About/>}/>
+            <Route path="/competencies" element={<Competencies/>}/>
+          </Routes>
+      </Router>
     </div>
   );
 }
