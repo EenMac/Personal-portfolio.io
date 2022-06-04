93a334579a8eec5bdb91605f405004c8aa8784a2
EenMacFullstack <Iain.mcdulling@gmail.com>
2022-06-04 11:41:46 +0100

    rendering image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import "../css/Home.css"
+import sunset from "../African-Sunset.jpeg";
 
 function Home() {
     return (
         <div id="home-container">
             <section id="header-section">
                 <h1>This will be the header component</h1>
+                <image src="../African-Sunset.jpeg"/>
             </section>
             <section id="content-section">
                 <h1>This will be the content section</h1>
