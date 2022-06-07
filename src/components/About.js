0f62e9a6870df4840e0a0c71e6568cf1f56fb70c
EenMacFullstack <Iain.mcdulling@gmail.com>
2022-06-07 08:30:27 +0100

    Likes and interests in about section added

--- src/components/About.js
+++ src/components/About.js
@@ -9,14 +9,38 @@ function About() {
           <h1>This is where the story will go</h1>
         </div>
       </section>
-      <section id="experience-section">
-        <div>
-          <h1>This is my work experience section</h1>
-        </div>
-      </section>
       <section id="interests-section">
         <div>
-          <h1>This is where my likes and interests will go</h1>
+          <h1>My Likes and interests</h1>
+          <div>
+            <ul>
+              <h2>Sports</h2>
+              <li>Rugby</li>
+              <li>Cricket</li>
+              <li>Gymnastics</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <h2>Topics</h2>
+              <li>Green tech</li>
+              <li>Politics and governance structures</li>
+              <li>Economics</li>
+              <li>Habits and psychology</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <h2>Activities</h2>
+              <li>Touch rugby</li>
+              <li>Snowboarding</li>
+              <li>Wakeboarding</li>
+              <li>Mountain-climbing</li>
+              <li>Gym</li>
+              <li>Hiking</li>
+              <li>Reading</li>
+            </ul>
+          </div>
         </div>
       </section>
     </div>
